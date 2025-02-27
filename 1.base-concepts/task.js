31646c1f0c0427a1800e69f41f1dc5ed62848c99
Karina Khaltunen <khaltunen@bk.ru>
2025-02-27 12:20:43 +0300

    solutions tasks 1 and 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
-function solveEquation(a, b, c) {
+
+function solveEquation (a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d = b**2-4*a*c;
+  if (d < 0) {
+    return arr;
+  }
+  if (d === 0) {
+    arr.push(-b/(2*a));
+    return arr;
+  }
+  if (d > 0) {
+    arr.push ((-b + Math.sqrt(d) )/(2*a));
+    arr.push ((-b - Math.sqrt(d) )/(2*a));
+    return arr;
+  }
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+function calculateTotalMortgage (percent, contribution, amount, countMonths) {
+  let monthlyPercent = percent / 100 / 12; //месячный процент
+  let loanBody = amount - contribution; //тело кредита
+  let monthlyPayment = loanBody* (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**countMonths - 1)))); // ежемесячный платеж
+  let totalAmount = parseFloat((monthlyPayment * countMonths).toFixed(2));
+  return totalAmount;  
 }
