c0d16417f5fd7205bf1a88555c8ea457c045d7fe
Karina Khaltunen <khaltunen@bk.ru>
2025-03-11 22:03:44 +0300

    change task 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -14,16 +14,16 @@ function getArrayParams(...arr) {
 	const avg = +(sum / arr.length).toFixed(2);
 	return {
 		min: min, 
-    max: max, 
-    avg: avg
+		max: max, 
+		avg: avg
 	}
 }
 console.log(getArrayParams(-99, 99, 10));
 console.log(getArrayParams(1, 2, 3, -100, 10));
 console.log(getArrayParams(5));
 
+//должна находить сумму элементов массива и возвращать её
 function summElementsWorker(...arr) {
-	/*должна находить сумму элементов массива и возвращать её.*/
 	if (arr.length === 0)
 		return 0;
 	let sum = 0;
@@ -33,13 +33,13 @@ function summElementsWorker(...arr) {
 	return sum;
 }
 
+//для вычисления разницы максимального и минимального элементов
 function differenceMaxMinWorker(...arr) {
-	/*для вычисления разницы максимального и минимального элементов*/
 	if (arr.length === 0)
 		return 0;
-	max = arr[i];
-	min = arr[i];
-	for (let i = 0; i < arr.length; i++) {
+	max = arr[0];
+	min = arr[0];
+	for (let i = 1; i < arr.length; i++) {
 		if (arr[i] < min) {
 			min = arr[i];
 		}
@@ -50,8 +50,8 @@ function differenceMaxMinWorker(...arr) {
 	return max - min;
 }
 
+//вычисления разницы сумм чётных и нечётных элементов
 function differenceEvenOddWorker(...arr) {
-	/*вычисления разницы сумм чётных и нечётных элементов*/
 	if (arr.length === 0)
 		return 0;
 	let sumEvenElement = 0;
@@ -66,8 +66,8 @@ function differenceEvenOddWorker(...arr) {
 	return sumEvenElement - sumOddElement;
 }
 
+//будет вычислять среднее значение чётных элементов
 function averageEvenElementsWorker() {
-	/**будет вычислять среднее значение чётных элементов */
 	if (arr.length === 0)
 		return 0;
 	let sumEvenElement = 0;
@@ -98,9 +98,9 @@ console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); //
 console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9)); // [2, 4, 6, 8] => 5
 console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // [64, 10, 40] => 38
 
+//из массива данных и насадки будет возвращать максимальный результат функции насадки
 function makeWork(arrOfArr, func) {
-	/**из массива данных и насадки будет возвращать максимальный результат функции насадки */
-	let maxWorkerResult = -Infinity; /**максимальный результат */
+	let maxWorkerResult = -Infinity;
 	for (let i = 0; i < arrOfArr.length; i++) {
 		const result = func(...arrOfArr[i]);
 		console.log(result);
