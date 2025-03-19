cffc3afb389149bcc3e86adf698742e448d06e3f
Karina Khaltunen <khaltunen@bk.ru>
2025-03-19 20:52:57 +0300

    add code tasks 1 & 2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length){
+        return false;
+    }
+    return arr1.every((item) => {return arr1 [item] === arr2 [item]});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const genderFilter = users.filter(user => user.gender === gender);
+    if (genderFilter.length === 0) {
+        return 0;
+    }
+    const ageUsers = genderFilter.map(user => user.age);
+    const avarageAge = (ageUsers.reduce((acc, age) => (acc + age)))/ageUsers.length;
+    return avarageAge;
 }
