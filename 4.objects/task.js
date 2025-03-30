8094d8874e64b3866afe96dc2e1dbd9c6aefbad7
Karina Khaltunen <khaltunen@bk.ru>
2025-03-30 22:52:40 +0300

    add code tast 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -7,7 +7,7 @@ function Student(name, gender, age) {
 
 Student.prototype.setSubject = function(subjectName) {
     this.subject = subjectName;
-    return ('subject: &{this.subjectName}')
+    return this.subject;
 }
 
 Student.prototype.addMarks = function(...marksToAdd) {
@@ -34,3 +34,9 @@ Student.prototype.exclude = function(reason) {
     delete this.marks;
     this.excluded = reason;
 }
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
