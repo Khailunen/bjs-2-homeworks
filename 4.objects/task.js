18f482080adeec709c7a31618aca09be3701a61e
Karina Khaltunen <khaltunen@bk.ru>
2025-03-30 22:35:23 +0300

    task 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = new Array();
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+    this.subjectName = subjectName;
+    return ('subject: &{this.subjectName}')
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+    if (!this.marks) {
+        return ("Студент отчислен");
+    }
+    if (!!marksToAdd.length) {
+        this.marks.push(...marksToAdd);
+    }
+    return;
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+    if (!this.marks || !this.marks.length) {
+        return 0;
+    }
+    const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+    const average = sum / this.marks.length;
+    return average;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
