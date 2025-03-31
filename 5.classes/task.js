ffcd6dad47c3eb1751786e09ca82512f00588c60
Karina Khaltunen <khaltunen@bk.ru>
2025-03-31 22:19:48 +0300

    add tasks

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -85,25 +85,3 @@ class Library {
 		return searchBookByName !== -1 ? this.books.splice(searchBookByName, 1) : null;
 	}
 }
-const library = new Library("Библиотека имени Ленина");
-
-library.addBook(
- new DetectiveBook(
-   "Артур Конан Дойл",
-   "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-   2019,
-   1008
- )
-);
-library.addBook(
-    new FantasticBook(
-      "Аркадий и Борис Стругацкие",
-      "Пикник на обочине",
-      1972,
-      168
-    )
-   );
-   library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
-   library.addBook(new Magazine("Мурзилка", 1924, 60));
-   
-   console.log(library.findBookBy("name", "Властелин колец")); //null
