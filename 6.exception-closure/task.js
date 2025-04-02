4d06ef251ff13e8dd1d222e535e4af205d498d57
Karina Khaltunen <khaltunen@bk.ru>
2025-04-02 22:17:35 +0300

    task 1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,18 @@
-﻿
+﻿function parseCount(value){
+    const valueParsing = Number.parseFloat(value);
+    if (isNan(valueParsing)){
+        const error =  new Error ("Невалидное значение");
+        throw error;
+    }
+    return valueParsing;
+}
+
+function validateCount(value){
+    try{
+        const valueParsing = parseCount(value);
+    } catch (error) {
+        return error;
+    }finally{
+        return valueParsing;
+    }
+}
